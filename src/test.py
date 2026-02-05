57cbb51b8e3edacd388beaf3cb3d9a4a1a087c2f
ekmountain <ekmountain@gmail.com>
2026-02-05 16:36:32 -0500

    Refactor clock in and clock out functions for clarity

--- src/test.py
+++ src/test.py
@@ -1,9 +1,9 @@
 # EM - Time Clock Application Basic Tasks
 
-# Clock in an employee
+# Function clock in an employee
+def clock_in(employee_id):
+    print(f"Employee {employee_id} clocked in.")
 
-# Clock out an employee
-
-# Employee break start
-
-# Employee break end 
+# Function clock out an employee
+def clock_out(employee_id):
+    print(f"Employee {employee_id} clocked out.")
