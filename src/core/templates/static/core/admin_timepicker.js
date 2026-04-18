9a0a7156897d8293e109fbf1625a92afa45e517a
Art "Artiphite <117311916+Artiphite@users.noreply.github.com>
2026-04-18 18:16:32 -0400

    Delete src/core directory

--- src/core/templates/static/core/admin_timepicker.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const timeInputs = document.querySelectorAll("input.vTimeField");
-  timeInputs.forEach((input) => {
-    input.setAttribute("type", "time");
-    input.step = 1;
-  });
-});
